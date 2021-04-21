a95a37b2223841ce595e73031e5c058dca9208a9
Fadia Rani <fadiarani9@gmail.com>
2021-04-21 16:42:17 +0700

    lanjutan

---
+++ app/src/main/java/com/example/kelasasqlite/adapter/TemanAdapter.java
@@ -0,0 +1,8 @@
+package com.example.kelasasqlite.adapter;
+
+import androidx.recylerview.widget.RecyclerView;
+
+public class TemanAdapter extends RecycleListView.Adapter<TemanAdapter.TemanViewHolder> {
+    public class TemanViewHolder {
+    }
+}
